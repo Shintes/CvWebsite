165b2ecc0b1e2d203aadd64c020e0250fafdac0a
Shintes <Sintescu.cristian@gmail.com>
2025-04-06 12:35:59 +0300

    Update SocialSection.tsx

--- src/Components/HomePage/SocialSection.tsx
+++ src/Components/HomePage/SocialSection.tsx
@@ -5,7 +5,7 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
 import { useNavigate } from "react-router-dom";
-import { titleStyles, subtitleStyles } from "./StyledComponents";
+import { titleStyles } from "./StyledComponents";
 
 const SocialSection = () => {
   const theme = useTheme();
