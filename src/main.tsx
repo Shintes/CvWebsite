bb198b0994e8bb8160ca6e788a41a30e4c0cde62
Shintes <Sintescu.cristian@gmail.com>
2025-04-06 22:54:38 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./Components/Services/Store/store.ts";
+import { store } from "./Components/Services/Store/store";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
