6df120b3d968a54006bd3a0227535eeff0cca519
Shintes <Sintescu.cristian@gmail.com>
2025-04-11 00:42:03 +0300

    added store/slice deleted local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,50 @@
 import Menu from "./Menu/Menu";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { lightTheme, darkTheme } from "./theme";
 import { AnimatePresence } from "framer-motion";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleTheme, setTheme } from "./store/themeSlice";
+import { RootState } from "./store/store";
 
-function App() {
-  const [darkMode, setDarkMode] = useState(() => {
-    const savedMode = localStorage.getItem("darkMode");
-    if (savedMode !== null) {
-      return JSON.parse(savedMode);
-    }
-    return window.matchMedia("(prefers-color-scheme: dark)").matches;
-  });
+function ThemeWrapper() {
+  const darkMode = useSelector((state: RootState) => state.theme.darkMode);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
     const handleChange = (e: MediaQueryListEvent) => {
-      setDarkMode(e.matches);
+      dispatch(setTheme(e.matches));
     };
 
     mediaQuery.addEventListener("change", handleChange);
-
-    localStorage.setItem("darkMode", JSON.stringify(darkMode));
     document.documentElement.style.colorScheme = darkMode ? "dark" : "light";
 
     return () => mediaQuery.removeEventListener("change", handleChange);
-  }, [darkMode]);
+  }, [darkMode, dispatch]);
 
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-        <CssBaseline />
-        <AnimatePresence mode="wait">
-          <Menu toggleDarkMode={() => setDarkMode(!darkMode)} />
-        </AnimatePresence>
-      </ThemeProvider>
-    </BrowserRouter>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <CssBaseline />
+      <AnimatePresence mode="wait">
+        <Menu toggleDarkMode={() => dispatch(toggleTheme())} />
+      </AnimatePresence>
+    </ThemeProvider>
+  );
+}
+
+function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeWrapper />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
